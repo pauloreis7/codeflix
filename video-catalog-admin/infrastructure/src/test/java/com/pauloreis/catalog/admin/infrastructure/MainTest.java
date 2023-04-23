447a590ebc78a7edf163253586179772f3071225
Paulo Reis <paulosilvadosreis2057@gmail.com>
2023-04-23 10:45:11 -0300

    feat(infrastructure): create infrastructure project sub module

---
+++ video-catalog-admin/infrastructure/src/test/java/com/pauloreis/catalog/admin/infrastructure/MainTest.java
@@ -0,0 +1,13 @@
+package com.pauloreis.catalog.admin.infrastructure;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;;
+
+public class MainTest {
+    @Test
+    public void testMain() {
+        assertNotNull(new Main());
+
+        Main.main(new String[] {});
+    }
+}
