297ba0bd935fda91298e35c00ad106f37a9045d8
Paulo Reis <paulosilvadosreis2057@gmail.com>
2023-04-23 10:44:42 -0300

    feat(application): create application project sub module

---
+++ video-catalog-admin/application/src/main/java/com/pauloreis/catalog/admin/application/UseCase.java
@@ -0,0 +1,10 @@
+package com.pauloreis.catalog.admin.application;
+
+import com.pauloreis.catalog.admin.domain.Category;
+
+public class UseCase {
+
+   public Category execute() {
+      return new Category();
+   }
+}
---
+++ video-catalog-admin/application/src/test/java/com/pauloreis/catalog/admin/application/UseCaseTest.java
@@ -0,0 +1,12 @@
+package com.pauloreis.catalog.admin.application;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class UseCaseTest {
+    @Test
+    public void testCreateUseCase() {
+        assertNotNull(new UseCase());
+        assertNotNull(new UseCase().execute());
+    }
+}
