446b9cf207766042f9c261d27410e490ee8bdb5c
Paulo Reis <paulosilvadosreis2057@gmail.com>
2023-04-29 11:20:46 -0300

    build(infrastucture): add spring boot to gradle project

--- video-catalog-admin/infrastructure/src/main/java/com/pauloreis/catalog/admin/infrastructure/Main.java
+++ video-catalog-admin/infrastructure/src/main/java/com/pauloreis/catalog/admin/infrastructure/Main.java
@@ -1,7 +1,13 @@
 package com.pauloreis.catalog.admin.infrastructure;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.pauloreis.catalog.admin.infrastructure.configuration.WebServerConfig;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
---
+++ video-catalog-admin/infrastructure/src/main/java/com/pauloreis/catalog/admin/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.pauloreis.catalog.admin.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.pauloreis.catalog.admin")
+public class WebServerConfig {
+}
