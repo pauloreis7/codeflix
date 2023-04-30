07341341a280ee661fbe4aef3838f8c02dadaa7a
Paulo Reis <paulosilvadosreis2057@gmail.com>
2023-04-30 11:35:42 -0300

    test(db): config datasource for dev and test envs

--- video-catalog-admin/infrastructure/src/test/java/com/pauloreis/catalog/admin/infrastructure/MainTest.java
+++ video-catalog-admin/infrastructure/src/test/java/com/pauloreis/catalog/admin/infrastructure/MainTest.java
@@ -1,11 +1,13 @@
 package com.pauloreis.catalog.admin.infrastructure;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertNotNull;;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.springframework.core.env.AbstractEnvironment;
 
 public class MainTest {
     @Test
     public void testMain() {
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "test");
         assertNotNull(new Main());
 
         Main.main(new String[] {});
