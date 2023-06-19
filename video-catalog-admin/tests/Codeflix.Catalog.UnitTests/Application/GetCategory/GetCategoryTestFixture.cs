452cccee91c3a815670f2a390cc106154fc9f065
Paulo Reis <paulosilvadosreis2057@gmail.com>
2023-06-19 06:52:39 -0300

    test(video-catalog-admin): create get category use case unit test

---
+++ video-catalog-admin/tests/Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryTestFixture.cs
@@ -0,0 +1,42 @@
+using Codeflix.Catalog.Domain.Entity;
+using Codeflix.Catalog.Domain.Repository;
+using Codeflix.Catalog.UnitTests.Domain.Entity.Common;
+using Moq;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.GetCategory;
+
+[CollectionDefinition(nameof(GetCategoryTestFixture))]
+public class GetCategoryTestFixtureCollection :
+    ICollectionFixture<GetCategoryTestFixture>
+{ }
+
+public class GetCategoryTestFixture : BaseFixture
+{
+  public Mock<ICategoryRepository> GetRepositoryMock()
+      => new();
+  public string GetValidCategoryName()
+  {
+    var categoryName = "";
+    while (categoryName.Length < 3)
+      categoryName = Faker.Commerce.Categories(1)[0];
+    if (categoryName.Length > 255)
+      categoryName = categoryName[..255];
+    return categoryName;
+  }
+
+  public string GetValidCategoryDescription()
+  {
+    var categoryDescription =
+        Faker.Commerce.ProductDescription();
+    if (categoryDescription.Length > 10_000)
+      categoryDescription =
+          categoryDescription[..10_000];
+    return categoryDescription;
+  }
+
+  public Category GetValidCategory()
+    => new(
+      GetValidCategoryName(),
+      GetValidCategoryDescription()
+    );
+}
