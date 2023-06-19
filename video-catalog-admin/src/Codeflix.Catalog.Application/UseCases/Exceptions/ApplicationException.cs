967d2ba0b6eabe69746ae4314955cd723bf41c12
Paulo Reis <paulosilvadosreis2057@gmail.com>
2023-06-19 08:18:12 -0300

    test(video-catalog-admin): create get category not found exception use case unit test

---
+++ video-catalog-admin/src/Codeflix.Catalog.Application/UseCases/Exceptions/ApplicationException.cs
@@ -0,0 +1,7 @@
+namespace Codeflix.Catalog.Application.Exceptions;
+
+public abstract class ApplicationException : Exception
+{
+  protected ApplicationException(string? message) : base(message)
+  { }
+}
--- video-catalog-admin/tests/Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryTest.cs
+++ video-catalog-admin/tests/Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryTest.cs
@@ -2,6 +2,7 @@ using Moq;
 using FC.Codeflix.Catalog.UnitTests.Application.GetCategory;
 using UseCase = Codeflix.Catalog.Application.UseCases.Category.GetCategory;
 using FluentAssertions;
+using Codeflix.Catalog.Application.Exceptions;
 
 namespace Codeflix.Catalog.UnitTests.Application.GetCategory;
 
@@ -42,5 +43,31 @@ public class GetCategoryTest
     output.IsActive.Should().Be(exampleCategory.IsActive);
     output.Id.Should().Be(exampleCategory.Id);
     output.CreatedAt.Should().Be(exampleCategory.CreatedAt);
+  }
+
+  [Fact(DisplayName = nameof(NotFoundExceptionWhenCategoryDoesntExist))]
+  [Trait("Application", "GetCategory - Use Cases")]
+  public async Task NotFoundExceptionWhenCategoryDoesntExist()
+  {
+    var repositoryMock = _fixture.GetRepositoryMock();
+    var exampleGuid = Guid.NewGuid();
+    repositoryMock.Setup(x => x.Get(
+        It.IsAny<Guid>(),
+        It.IsAny<CancellationToken>()
+    )).ThrowsAsync(
+        new NotFoundException($"Category '{exampleGuid}' not found")
+    );
+    var input = new UseCase.GetCategoryInput(exampleGuid);
+    var useCase = new UseCase.GetCategory(repositoryMock.Object);
+
+    var task = async ()
+        => await useCase.Handle(input, CancellationToken.None);
+
+    await task.Should().ThrowAsync<NotFoundException>();
+
+    repositoryMock.Verify(x => x.Get(
+        It.IsAny<Guid>(),
+        It.IsAny<CancellationToken>()
+    ), Times.Once);
   }
 }
