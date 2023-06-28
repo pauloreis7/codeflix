6d4514b436a447a534576d5d4b74b1f81935f9ed
Paulo Reis <paulosilvadosreis2057@gmail.com>
2023-06-28 07:06:46 -0300

    test(video-catalog-admin): add delete category use case integration test

---
+++ video-catalog-admin/tests/Codeflix.Catalog.IntegrationTests/Application/UseCases/Category/DeleteCategory/DeleteCategoryTestFixture.cs
@@ -0,0 +1,11 @@
+using Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.Common;
+
+namespace Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.DeleteCategory;
+
+[CollectionDefinition(nameof(DeleteCategoryTestFixture))]
+public class DeleteCategoryTestFixtureCollection
+  : ICollectionFixture<DeleteCategoryTestFixture>
+{ }
+
+public class DeleteCategoryTestFixture : CategoryUseCasesBaseFixture
+{ }
