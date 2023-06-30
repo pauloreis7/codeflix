eff9c5d5324a03d693b091c6694dbfdc9ab995d1
Paulo Reis <paulosilvadosreis2057@gmail.com>
2023-06-30 06:27:37 -0300

    build(video-catalog-admin): setup api dependency injection

---
+++ video-catalog-admin/src/Codeflix.Catalog.Api/Configurations/UseCasesConfiguration.cs
@@ -0,0 +1,29 @@
+using Codeflix.Catalog.Application.Interfaces;
+using Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+using Codeflix.Catalog.Domain.Repository;
+using Codeflix.Catalog.Infra.Data.EF;
+using Codeflix.Catalog.Infra.Data.EF.Repositories;
+using MediatR;
+
+namespace Codeflix.Catalog.Api.Configurations;
+
+public static class UseCasesConfiguration
+{
+  public static IServiceCollection AddUseCases(
+    this IServiceCollection services
+  )
+  {
+    services.AddMediatR(typeof(CreateCategory));
+    services.AddRepositories();
+    return services;
+  }
+
+  private static IServiceCollection AddRepositories(
+    this IServiceCollection services
+  )
+  {
+    services.AddTransient<ICategoryRepository, CategoryRepository>();
+    services.AddTransient<IUnitOfWork, UnitOfWork>();
+    return services;
+  }
+}
