b274323ea483e4bf82888dddc8eff08271870017
Paulo Reis <paulosilvadosreis2057@gmail.com>
2023-06-30 06:57:48 -0300

    build(video-catalog-admin): setup api db connection

---
+++ video-catalog-admin/src/Codeflix.Catalog.Api/Configurations/ConnectionsConfiguration.cs
@@ -0,0 +1,28 @@
+using Codeflix.Catalog.Infra.Data.EF;
+using Microsoft.EntityFrameworkCore;
+
+namespace Codeflix.Catalog.Api.Configurations;
+
+public static class ConnectionsConfiguration
+{
+  public static IServiceCollection AddAppConections(
+    this IServiceCollection services
+  )
+  {
+    services.AddDbConnection();
+    return services;
+  }
+
+  private static IServiceCollection AddDbConnection(
+    this IServiceCollection services
+  )
+  {
+    services.AddDbContext<CodeflixCatalogDbContext>(
+      options => options.UseInMemoryDatabase(
+        "InMemory-DSV-Database"
+      )
+    );
+
+    return services;
+  }
+}
