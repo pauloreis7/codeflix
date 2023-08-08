a3d22455258e31b5343234d6f63f5560d8e1f694
Paulo Reis <paulosilvadosreis2057@gmail.com>
2023-08-08 06:30:40 -0300

    feat(video-catalog-admin): add genre remove category entity

--- video-catalog-admin/src/Codeflix.Catalog.Domain/Entity/Genre.cs
+++ video-catalog-admin/src/Codeflix.Catalog.Domain/Entity/Genre.cs
@@ -47,6 +47,11 @@ public class Genre
     Validate();
   }
 
+  public void RemoveCategory(Guid categoryId)
+  {
+    _categories.Remove(categoryId);
+    Validate();
+  }
 
   private void Validate()
     => DomainValidation.NotNullOrEmpty(Name, nameof(Name));
--- video-catalog-admin/tests/Codeflix.Catalog.UnitTests/Domain/Entity/Genre/GenreTestFixture.cs
+++ video-catalog-admin/tests/Codeflix.Catalog.UnitTests/Domain/Entity/Genre/GenreTestFixture.cs
@@ -13,6 +13,17 @@ public class GenreTestFixture : BaseFixture
   public string GetValidName()
     => Faker.Commerce.Categories(1)[0];
 
-  public DomainEntity.Genre GetExampleGenre(bool isActive = true)
-     => new(GetValidName(), isActive);
+  public DomainEntity.Genre GetExampleGenre(
+    bool isActive = true,
+    List<Guid>? categoriesIdsList = null
+  )
+  {
+    var genre = new DomainEntity.Genre(GetValidName(), isActive);
+
+    if (categoriesIdsList is not null)
+      foreach (var categoryId in categoriesIdsList)
+        genre.AddCategory(categoryId);
+
+    return genre;
+  }
 }
